d2bb5f64bbee9f399e938587ce947d6336c6bbf0
mjkang25 <mj.kang@ewha.ac.kr>
2026-05-28 14:20:08 +0900

    feat: modify dbconnector

--- src/db/DatabaseConnector.java
+++ src/db/DatabaseConnector.java
@@ -1,21 +1,43 @@
 package db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnector {
 
     // MariaDB 연결 정보
-    private static final String URL      = "jdbc:mariadb://localhost:3306/conmayo"; // DB URL
-    private static final String USER     = "root";       // DB 계정
-    private static final String PASSWORD = "비밀번호입력";  // DB 비밀번호
+	private static String URL;
+    private static String USER;
+    private static String PASSWORD;
 
-    // 외부에서 인스턴스 생성 못하게 막음 (유틸 클래스)
+    static {
+    	
+    	Properties props = new Properties();
+    	
+    	try (FileInputStream fis =
+    	         new FileInputStream("src/db/db.properties")) {
+
+    	    props.load(fis);
+
+    	    URL = props.getProperty("url");
+    	    USER = props.getProperty("user");
+    	    PASSWORD = props.getProperty("password");
+
+    	} catch (IOException e) {
+    	    e.printStackTrace();
+    	}
+        
+    }
+
+    // 외부에서 인스턴스 생성 방지 (유틸 클래스)
     private DatabaseConnector() {}
 
-    // Connection 객체 반환 — 호출할 때마다 새 연결 생성
-    // DAOImpl 생성자에서 Main이 이 메서드로 conn을 받아서 주입
+    // Connection 객체 반환
+    // 생성된 Connection은 DAOImpl 생성자 등에 전달하여 사용
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
